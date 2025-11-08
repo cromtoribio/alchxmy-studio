3084cca808b20e18a693b0cdfd169d58c86c80d8
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2025-11-07 19:07:53 -0800

    Finished landing page v0.1.0

--- app/page.js
+++ app/page.js
@@ -1,66 +1,110 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+    let timeOfDay = "";
+    let timeEmoji = "";
+
+    const hour = new Date().getHours();
+
+    if (hour >= 5 && hour < 12) {
+        timeOfDay = "morning";
+        timeEmoji = "☀️";
+    } else if (hour >= 12 && hour < 17) {
+        timeOfDay = "afternoon";
+        timeEmoji = "♥️";
+    } else {
+        timeOfDay = "evening";
+        timeEmoji = "🌙";
+    }
+
     return (
-        <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-                <Image
-                    className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={100}
-                    height={20}
-                    priority
-                />
-                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-                        Update to check the git config!
-                    </h1>
-                    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-                        Looking for a starting point or more instructions? Head
-                        over to{" "}
-                        <a
-                            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
-                        >
-                            Templates
-                        </a>{" "}
-                        or the{" "}
-                        <a
-                            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                            className="font-medium text-zinc-950 dark:text-zinc-50"
-                        >
-                            Learning
-                        </a>{" "}
-                        center.
-                    </p>
+        <>
+            {/* Start Main Content */}
+            <div className="h-screen">
+                <div className="h-full sm:rounded-lg bg-linear-[-20deg,#1E261F10_20%,#111511_80%] flex flex-col justify-between sm:p-4 p-2">
+                    <div className="flex justify-between items-center space-x-4 max-sm:text-[10px] text-center sm:px-3 sm:pb-3 sm:pt-1 p-4 font-inter font-light">
+                        <Link href="/captains-log">v0.1.0</Link>
+                        <div className="h-px w-full bg-[#fff8dc50]"></div>
+                        <span className="min-w-fit">
+                            <em>brand experience architecture</em>
+                        </span>
+                    </div>
+                    <div className="flex justify-center items-center w-full h-full px-3">
+                        <main className="flex flex-col justify-center items-center sm:text-xl text-md h-full w-full lg:border-x lg:border-[#fff8dc50]">
+                            <div className="max-w-[500px]">
+                                <div className="text-center">
+                                    <Image
+                                        src="/FullLogo_Stacked_Light.png"
+                                        alt="Alchxmy Studio Logo"
+                                        width={250}
+                                        height={250}
+                                        className="mx-auto"
+                                    />
+                                    <p className="pt-4">
+                                        We are still setting the foundations of
+                                        our new home, but{" "}
+                                        <em>please feel free to explore!</em>
+                                    </p>
+                                </div>
+                                <div className="pt-5 w-full">
+                                    <Link
+                                        href="#"
+                                        className="flex justify-between py-3 px-5 bg-[#1E261F] border-black border rounded-lg drop-shadow-md/50 mt-2"
+                                    >
+                                        <span>Portfolio Adventure</span>
+                                        <Image
+                                            src="/rightarrow.svg"
+                                            alt="Arrow"
+                                            width={20}
+                                            height={20}
+                                        />
+                                    </Link>
+                                    <Link
+                                        href="#"
+                                        className="flex justify-between py-3 px-5 bg-[#1E261F] border-black border rounded-lg drop-shadow-md/50 mt-3 opacity-50"
+                                    >
+                                        <span>About the Studio</span>
+                                        <Image
+                                            src="/rightarrow.svg"
+                                            alt="Arrow"
+                                            width={20}
+                                            height={20}
+                                        />
+                                    </Link>
+                                </div>
+                            </div>
+                        </main>
+                    </div>
+                    <footer className="font-inter font-light">
+                        <div className="flex justify-between items-center space-x-4 max-sm:text-[10px] text-center sm:pt-4 sm:px-2 sm:pb-1 p-4">
+                            <span className="min-w-fit">
+                                <em>good {timeOfDay} from los angeles </em>
+                                {timeEmoji}
+                            </span>
+                            <div className="h-px w-full bg-[#fff8dc50]"></div>
+                            <div className="flex space-x-4 min-w-fit">
+                                <Link href="https://github.com/cromtoribio/alchxmy-studio">
+                                    <Image
+                                        src="/github.svg"
+                                        alt="Github"
+                                        width={20}
+                                        height={20}
+                                    />
+                                </Link>
+                                <Link href="https://www.instagram.com/alchxmystudio">
+                                    <Image
+                                        src="/instagram.svg"
+                                        alt="Github"
+                                        width={20}
+                                        height={20}
+                                    />
+                                </Link>
+                            </div>
+                        </div>
+                    </footer>
                 </div>
-                <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-                    <a
-                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={16}
-                            height={16}
-                        />
-                        Deploy Now
-                    </a>
-                    <a
-                        className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Documentation
-                    </a>
-                </div>
-            </main>
-        </div>
+            </div>
+        </>
     );
 }
